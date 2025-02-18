b1db187fa95d6b05d16098a93148e87f90a083b5
rinker2 <143932873+rinker2@users.noreply.github.com>
2025-02-18 23:16:44 +0400

    Update main.js
    Data updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
+// Данные
+// Пн
+let name11 = `Методы оптимизации (Лекция)`;
+let cab11 = 115;
+n11.textContent = name11;
+c11.textContent = cab11;
+
+let name12 = `Методы оптимизации (Семинар)`;
+let cab12 = 327;
+n12.textContent = name12;
+c12.textContent = cab12;
+
+let name13 = `Веб-технологии (Лекция)`;
+let cab13 = 323;
+n13.textContent = name13;
+c13.textContent = cab13;
+
+// Вт
+let name21 = `Английский язык B1 (Семинар)`;
+let cab21 = 32;
+n21.textContent = name21;
+c21.textContent = cab21;
+
+let name22 = `Английский язык B1 (Семинар)`;
+let cab22 = 32;
+n22.textContent = name22;
+c22.textContent = cab22;
+
+// Ср
+let name31 = `Криптография (Лекция)`;
+let cab31 = 418;
+n31.textContent = name31;
+c31.textContent = cab31;
+
+let name32 = `Веб-технологии (Семинар)`;
+let cab32 = 331;
+n32.textContent = name32;
+c32.textContent = cab32;
+
+// Чт
+let name41 = `Безопасное программирование (Лекция)`;
+let cab41 = 323;
+n41.textContent = name41;
+c41.textContent = cab41;
+
+let name42 = `Безопасное программирование (Семинар)`;
+let cab42 = 324;
+n42.textContent = name42;
+c42.textContent = cab42;
+
+let name43 = `Криптография (Семинар)`;
+let cab43 = 316;
+n43.textContent = name43;
+c43.textContent = cab43;
+
+let end1 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 15, 20);
+end1 = end1.getTime() - midnight;
+let end2 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 16, 50);
+end2 = end2.getTime() - midnight;
+let end3 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 18, 20);
+end3 = end3.getTime() - midnight;
+
+
 setInterval(() => {
 	let d = new Date();
-	// d = new Date(d.getFullYear(), d.getMonth(), 4, 9, 0, 0);
+	// d = new Date(d.getFullYear(), d.getMonth(), 19, 19, 0, 0);
 	let midnight = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0);
 	midnight = midnight.getTime();
 	let now_ms = d.getTime();
@@ -10,81 +73,6 @@ setInterval(() => {
 	let time = d.toLocaleTimeString('ru-RU');
 	dt.textContent = `${weekday} ${date} ${time}`;
 	function cabinet(weekday, midnight, ms_inday){
-		// Данные
-		// Пн
-		let name11 = `Языки программирования и компиляторы (Лекция)`;
-		n11.textContent = name11;
-		let cab11 = 317;
-		c11.textContent = cab11;
-		let name12 = `Теория вероятности и математическая статистика (Лекция)`;
-		n12.textContent = name12;
-		let cab12 = 316;
-		c12.textContent = cab12;
-		let name13 = `Теория вероятности и математическая статистика (Семинар)`;
-		n13.textContent = name13;
-		let cab13 = 319;
-		c13.textContent = cab13;
-
-		// Вт
-		let name21 = `Английский язык B1 (Семинар)`;
-		n21.textContent = name21;
-		let cab21 = 410;
-		c21.textContent = cab21;
-		let name22 = `Английский язык B1 (Семинар)`;
-		n22.textContent = name22;
-		let cab22 = 410;
-		c22.textContent = cab22;
-		let name23 = `Комплексный анализ (Лекция)`;
-		n23.textContent = name23;
-		let cab23 = 427;
-		c23.textContent = cab23;
-
-		// Ср
-		let alt = true;
-		
-		let name321 = `Комплексный анализ (Семинар)`;
-		n321.textContent = name321;
-		let cab321 = 319;
-		c321.textContent = cab321;
-		let name322 = `Физика (Лаборатория)`;
-		n322.textContent = name322;
-		let cab322 = 319;
-		c322.textContent = cab322;
-		let name33 = `Языки программирования и компиляторы (Семинар)`;
-		n33.textContent = name33;
-		let cab33 = 319;
-		c33.textContent = cab33;
-
-		// Чт
-		// let name41 = `Операционные системы (Лекция)`;
-		// n41.textContent = name41;
-		// let cab41 = 323;
-		// c41.textContent = cab41;
-		// let name42 = `Операционные системы (Семинар)`;
-		// n42.textContent = name42;
-		// let cab42 = 318;
-		// c42.textContent = cab42;
-
-		// Пт
-		let name51 = `Физика (Лекция)`;
-		n51.textContent = name51;
-		let cab51 = 318;
-		c51.textContent = cab51;
-		let name52 = `Анализ алгоритмов и меторы их построения (Лекция)`;
-		n52.textContent = name52;
-		let cab52 = 320;
-		c52.textContent = cab52;
-		let name53 = `Анализ алгоритмов и меторы их построения (Семинар)`;
-		n53.textContent = name53;
-		let cab53 = 318;
-		c53.textContent = cab53;
-
-		let end1 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 9, 50);
-		end1 = end1.getTime() - midnight;
-		let end2 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 11, 30);
-		end2 = end2.getTime() - midnight;
-		let end3 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 50);
-		end3 = end3.getTime() - midnight;
 		// Применение данных
 		let cab;
 		let name;
@@ -109,37 +97,25 @@ setInterval(() => {
 			cab = cab22;
 			name = name22;
 		}
-		else if (weekday == `вт` && ms_inday < end3) {
-			cab = cab23;
-			name = name23;
+		else if (weekday == `ср` && ms_inday < end1) {
+			cab = cab31;
+			name = name31;
 		}
 		else if (weekday == `ср` && ms_inday < end2) {
-			if (alt){
-				cab = cab321;
-				name = name321;
-				note = `Верхняя неделя (Alt həftə)`;
-			}
-			else{
-				cab = cab322;
-				name = name322;
-				note = `Нижняя неделя (Üst həftə)`;
-			}
+			cab = cab32;
+			name = name32;
 		}
-		else if (weekday == `ср` && ms_inday < end3) {
-			cab = cab33;
-			name = name33;
+		else if (weekday == 'чт' && ms_inday < end1) {
+			name = name41;
+			cab = cab41;
 		}
-		else if (weekday == 'пт' && ms_inday < end1) {
-			name = name51;
-			cab = cab51;
+		else if (weekday == 'чт' && ms_inday < end2) {
+			name = name42;
+			cab = cab42;
 		}
-		else if (weekday == 'пт' && ms_inday < end2) {
-			name = name52;
-			cab = cab52;
-		}
-		else if (weekday == 'пт' && ms_inday < end3) {
-			name = name53;
-			cab = cab53;
+		else if (weekday == 'чт' && ms_inday < end3) {
+			name = name43;
+			cab = cab43;
 		}
 		else {
 			cab = 0;
@@ -149,13 +125,13 @@ setInterval(() => {
 
 		let period;
 		if (ms_inday < end1 && cab != 0) {
-			period = `[8:30 - 9:50]`;
+			period = `[14:00 - 15:20]`;
 		}
 		else if (ms_inday < end2 && cab != 0) {
-			period = `[10:00 - 11:20]`;
+			period = `[15:30 - 16:50]`;
 		}
 		else if (ms_inday < end3 && cab != 0) {
-			period = `[11:30 - 12:50]`;
+			period = `[17:00 - 18:20]`;
 		}
 		else {
 			period = ` `;
@@ -167,6 +143,7 @@ setInterval(() => {
 	period.textContent = cabinet(weekday, midnight, ms_inday)[2];
 	note.textContent = cabinet(weekday, midnight, ms_inday)[3];
 })
+
 
 // Открытие модального окна "Что это?"
 document.getElementById("open-about-btn").addEventListener("click", function(){
